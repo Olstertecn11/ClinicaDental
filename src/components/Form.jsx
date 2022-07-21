acbb7baceb166f3b23206d16b9769afd09ca6c92
Olivers11 <olstertecn597@gmail.com>
2022-07-20 19:55:05 -0600

    initializing with form component before custom navbar

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,23 +9,23 @@ export default function Form() {
         <>
             <div className="container mt-4">
                 <div className="row">
-                    <div className="col">
-                        <div className="card card-body mt-4">
+                    <div className="col-md-4">
+                        <div className="card card-body mt-4 shadow p-3 mb-5 bg-white rounded">
                             <div className="form-group">
                                 <label htmlFor="name">Nombre</label>
-                                <input type="text" placeholder="Ingrese su nombre"  id="name" className="form-control" />
+                                <input type="text" placeholder="Ingrese su nombre" autoComplete='none' id="name" className="form-control" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="name">Telefono</label>
-                                <input type="text" placeholder="Ingrese su nombre"  id="name" className="form-control" />
+                                <input type="text" placeholder="Ingrese su nombre" autoComplete='none' id="name" className="form-control" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="name">Email</label>
-                                <input type="email" placeholder="Ingrese su nombre"  id="name" className="form-control" />
+                                <input type="email" placeholder="Ingrese su nombre"  autoComplete='none'id="name" className="form-control" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="name">Mensaje</label>
-                                <textarea className="form-control" id="mensaje" cols="30" rows="10"></textarea>
+                                <textarea className="form-control" id="mensaje" autoComplete='none' cols="30" rows="10"></textarea>
                             </div>
                             <div className="form-group">
                                 <button className="btn btn-block">
